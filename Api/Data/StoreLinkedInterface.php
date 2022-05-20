64df9ae8e84769111230f74510c81c28075b7623
Sander Merks <merks@weprovide.com>
2022-05-20 09:55:00 +0200

    Adding more flexibility to the abstract classes, and adding interface

---
+++ Api/Data/StoreLinkedInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeProvide\Core\Api\Data;
+
+/**
+ * Interface StoreLinkedInterface
+ *
+ * Below values are all provided in the WeProvide\Core\Model\ResourceModel\AbstractStoreLinkedCollection
+ * performAfterLoad();
+ *
+ * @method array getStoreId()
+ * @method StoreLinkedInterface setStoreId(array $storeId)
+ * @method string getStoreCode()
+ * @method StoreLinkedInterface setStoreCode(string $storeCode)
+ */
+interface StoreLinkedInterface
+{
+}
