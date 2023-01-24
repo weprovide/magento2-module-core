a9b2291a9c2b2d23472c9be326a49aa3e6253d9a
Sander Merks <sander.merks@iodigital.com>
2023-01-24 13:38:54 +0100

    Add set method in both CurrentCategory and CurrentProduct

--- Service/CurrentProduct.php
+++ Service/CurrentProduct.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace WeProvide\Core\Service;
 
-use Magento\Catalog\Model\Product;
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Framework\Registry;
 
 class CurrentProduct
 {
+    /** @var string */
+    protected const CURRENT_PRODUCT = 'current_product';
+
     /** @var Registry */
     protected $registry;
 
@@ -22,10 +25,25 @@ class CurrentProduct
     }
 
     /**
-     * @return Product|null
+     * Gets the current product
+     *
+     * @return ProductInterface|null
      */
-    public function getCurrentProduct(): ?Product
+    public function getCurrentProduct(): ?ProductInterface
     {
-        return $this->registry->registry('current_product');
+        return $this->registry->registry(static::CURRENT_PRODUCT);
+    }
+
+    /**
+     * Sets the current product
+     *
+     * @param ProductInterface|null $product
+     * @return CurrentProduct
+     */
+    public function setCurrentProduct(?ProductInterface $product): CurrentProduct
+    {
+        $this->registry->register(static::CURRENT_PRODUCT, $product);
+
+        return $this;
     }
 }
